ed3f23bd412a8292bf224e3e3401579ef43041e5
cameronjacoby <cameron.jacoby@gmail.com>
2014-08-30 13:24:37 -0700

    add price data for other cities to seed data

--- spec/models/price_query_spec.rb
+++ spec/models/price_query_spec.rb
@@ -2,14 +2,13 @@ require 'rails_helper'
 
 RSpec.describe PriceQuery, :type => :model do
 	it "should save a price query to the database" do
-		example_query_ga = PriceQuery.create({
+		example_query_sf = PriceQuery.create({
 			:start_latitude => 37.786958,
       :start_longitude => -122.394462,
-      :end_latitude => 37.777034,
-      :end_longitude => -122.4152628,
+      :end_latitude => 37.787933,
+      :end_longitude => -122.4074981,
       :city => "San Francisco"
 	})
-		expect(example_query_ga).not_to eq(nil)
+		expect(example_query_sf).not_to eq(nil)
 	end
 end
-
