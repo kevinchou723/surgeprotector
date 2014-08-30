c93dc5d64dde6647d27dedf5554be66f2ea6184c
PAUCHOU <PAUCHOU@Kevin-Chous-MacBook-Pro.local>
2014-08-30 12:54:18 -0700

    api calls added

--- app/models/price_query.rb
+++ app/models/price_query.rb
@@ -1,11 +1,8 @@
 class PriceQuery < ActiveRecord::Base
-	validates :start_latitude, :presence => true, :numericality => { :greater_than_or_equal_to => -90, :less_than_or_equal_to =>90}
-	
-	validates :start_longitude, :presence => true,:numericality => { :greater_than_or_equal_to => -180, :less_than_or_equal_to =>180}
-	
-	validates :end_latitude, :presence => true, :numericality => { :greater_than_or_equal_to => -90, :less_than_or_equal_to =>90}
-	
-	validates :end_longitude, :presence => true, :numericality => { :greater_than_or_equal_to => -180, :less_than_or_equal_to =>180}
+	validates :start_latitude, :presence => true, :numericality => { :greater_than_or_equal_to => -90, :less_than_or_equal_to => 90}
+	validates :start_longitude, :presence => true,:numericality => { :greater_than_or_equal_to => -180, :less_than_or_equal_to => 180}
+	validates :end_latitude, :presence => true, :numericality => { :greater_than_or_equal_to => -90, :less_than_or_equal_to => 90}
+	validates :end_longitude, :presence => true, :numericality => { :greater_than_or_equal_to => -180, :less_than_or_equal_to => 180}
 	
 	has_many :price_results
 end
