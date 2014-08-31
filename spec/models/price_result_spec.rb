a1246617e9c43e65673494a817e536e6e36bd651
cameronjacoby <cameron.jacoby@gmail.com>
2014-08-31 16:06:38 -0700

    add tests to price result model to test belongs_to association

--- spec/models/price_result_spec.rb
+++ spec/models/price_result_spec.rb
@@ -2,16 +2,15 @@ require 'rails_helper'
 
 RSpec.describe PriceResult, :type => :model do
 
-	it "should save results to the database and associate it with price query" do
-		example_query_sf = PriceQuery.create({
-			:start_latitude => 37.786958,
+  before(:each) do
+    @example_query_sf = PriceQuery.create({
+      :start_latitude => 37.786958,
       :start_longitude => -122.394462,
       :end_latitude => 37.787933,
       :end_longitude => -122.4074981,
       :city => "San Francisco"
     })
-
-  	example_query_sf.price_results.create({
+    @example_price_result_sf = @example_query_sf.price_results.create({
       "localized_display_name"=>"uberX",
       "low_estimate"=>"5",
       "display_name"=>"uberX",
@@ -21,9 +20,15 @@ RSpec.describe PriceResult, :type => :model do
       "high_estimate"=>"7",
       "currency_code"=>"USD"
     })
+  end
 
-  	expect(example_query_sf.price_results).not_to eq(nil)
-
+	it "should save price result to the database and associate it with price query" do
+    expect(@example_price_result_sf).not_to eql(nil)
+  	expect(@example_query_sf.price_results).not_to eql(nil)
 	end
+
+  it "should belong to price query" do
+    expect(@example_price_result_sf.price_query).not_to eql(nil)
+  end
 
 end
