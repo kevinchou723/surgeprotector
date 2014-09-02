696a1d37beb9c99185e417ab8f1244ba6ea79a13
PAUCHOU <PAUCHOU@Kevin-Chous-MacBook-Pro.local>
2014-09-02 14:55:13 -0700

    big update

--- app/models/price_query.rb
+++ app/models/price_query.rb
@@ -1,13 +1,13 @@
 class PriceQuery < ActiveRecord::Base
-<<<<<<< HEAD
+
 	validates :start_latitude, :presence => true, :numericality => { :greater_than_or_equal_to => -90, :less_than_or_equal_to => 90}
 	validates :start_longitude, :presence => true,:numericality => { :greater_than_or_equal_to => -180, :less_than_or_equal_to => 180}
 	validates :end_latitude, :presence => true, :numericality => { :greater_than_or_equal_to => -90, :less_than_or_equal_to => 90}
 	validates :end_longitude, :presence => true, :numericality => { :greater_than_or_equal_to => -180, :less_than_or_equal_to => 180}
 	
 	has_many :price_results
-end
-=======
+
+
 
   has_many :price_results
   has_many :averages
@@ -41,4 +41,4 @@ end
     }
 
 end
->>>>>>> c55e04610d70615bb223da223f2593e0b18c35fc
+
