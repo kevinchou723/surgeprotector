b6c7b1caa1228d9c46db1fb4458e5e09c42c8668
cameronjacoby <cameron.jacoby@gmail.com>
2014-09-03 15:26:53 -0700

    remove old search logic from site.js

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -18,31 +18,6 @@ $(document).ready(function() {
     });
 
   });
-  
-
-  // LOGIC FOR SEARCH QUERY
-
-  // var hours = [];
-  // var surges = [];
-  // var data = [];
-
-  // $('.hour').each(function() {
-  //   hours.push($(this).html());
-  // });
-
-  // $('.surge').each(function() {
-  //   surges.push(parseFloat($(this).html()));
-  // });
-
-  // // format each data point into an object and push into the data array
-  // for (i = 0; i < hours.length; i += 1) {
-  //   var obj = {};
-  //   obj.hour = hours[i];
-  //   obj.surge = surges[i];
-  //   data.push(obj);
-  // }
-
-  // console.log(data);
 
 
   // CHART.JS LOGIC BELOW
