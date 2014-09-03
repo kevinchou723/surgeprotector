1dbdd17168eb4ed51f6fa1ef79c142b79f6276f8
cameronjacoby <cameron.jacoby@gmail.com>
2014-09-03 09:33:19 -0700

    add flash messages for user login

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,28 +1,29 @@
 class SessionController < ApplicationController
 
   def new
-  	#login form
   	if session[:user_id] != nil
   		redirect_to root_path
   	end
   end
 
   def create
-  	# raise params.inspect
   	@user = User.authenticate(params[:user][:email], params[:user][:password])
-  	# raise @user
   	if @user 
   		session[:user_id] = @user.id
-  		redirect_to root_path
+  		redirect_to user_path(@user.id), :notice => "Logged in!"
   	else
-  		#add flash message here
-  		redirect_to login_path
+      if User.find_by_email(params[:user][:email]) == nil
+  		  redirect_to login_path, :notice => "Account with that email does not exist."
+      elsif User.find_by_email(params[:user][:email]).password != params[:user][:password]
+        redirect_to login_path, :notice => "Invalid password."
+      else
+        redirect_to login_path, :notice => "Could not log you in. Please try again."
+      end
   	end
   end
 
   def destroy
   	session[:user_id] = nil
-  	# add flash message here "logged out"
-  	redirect_to login_path
+  	redirect_to login_path, :notice => "Logged out."
   end
 end
