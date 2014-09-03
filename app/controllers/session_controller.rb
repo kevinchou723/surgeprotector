a0c5c3e4154b2c5199c43ec5d456a8bb014a43c7
cameronjacoby <cameron.jacoby@gmail.com>
2014-09-03 10:34:53 -0700

    scope all user functionality to current_user

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -10,20 +10,20 @@ class SessionController < ApplicationController
   	@user = User.authenticate(params[:user][:email], params[:user][:password])
   	if @user 
   		session[:user_id] = @user.id
-  		redirect_to user_path(@user.id), :notice => "Logged in!"
+  		redirect_to user_path(@user.id), :notice => 'Logged in!'
   	else
       if User.find_by_email(params[:user][:email]) == nil
-  		  redirect_to login_path, :notice => "Account with that email does not exist."
+  		  redirect_to login_path, :notice => 'Account with that email does not exist.'
       elsif User.find_by_email(params[:user][:email]).password != params[:user][:password]
-        redirect_to login_path, :notice => "Invalid password."
+        redirect_to login_path, :notice => 'Invalid password.'
       else
-        redirect_to login_path, :notice => "Could not log you in. Please try again."
+        redirect_to login_path, :notice => 'Could not log you in. Please try again.'
       end
   	end
   end
 
   def destroy
   	session[:user_id] = nil
-  	redirect_to login_path, :notice => "Logged out."
+  	redirect_to login_path, :notice => 'Logged out.'
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,67 +3,95 @@ class UsersController < ApplicationController
   before_action :is_authenticated?, except: [:new, :create]
 
   def new
-    @user = User.new
+    if session[:user_id] != nil
+      redirect_to user_path(current_user.id)
+    else
+      @user = User.new
+    end
   end
 
   def create
-    new_user = params.require(:user).permit(
-      :first_name,
-      :last_name,
-      :email,
-      :password,
-      :password_confirmation
-    )
-
-    @user = User.new(new_user)
-
-    if @user.save
-      auth_user = User.authenticate(@user.email, @user.password)
-      if auth_user
-        session[:user_id] = @user.id
-        redirect_to user_path(@user.id), :notice => "Profile created!"
+    if session[:user_id] == nil
+      new_user = params.require(:user).permit(
+        :first_name,
+        :last_name,
+        :email,
+        :password,
+        :password_confirmation
+      )
+      @user = User.new(new_user)
+      if @user.save
+        auth_user = User.authenticate(@user.email, @user.password)
+        if auth_user
+          session[:user_id] = @user.id
+          redirect_to user_path(@user.id), :notice => 'Profile created!'
+        end
+      else
+        if User.find_by_email(@user.email)
+          redirect_to signup_path, :notice => 'An account with that email already exists.'
+        elsif @user.password.length < 6
+          redirect_to signup_path, :notice => 'Password must be at least 6 characters.'
+        elsif @user.password != @user.password_confirmation
+          redirect_to signup_path, :notice => 'Passwords must match.'
+        end
       end
     else
-      if User.find_by_email(@user.email)
-        redirect_to signup_path, :notice => "An account with that email already exists."
-      end
+      session[:user_id] = nil
+      redirect_to login_path, :notice => 'Please re-login.'
     end
   end
 
   def show
-    user_id = params[:id]
-    @user = User.find_by_id(user_id)
-    @price_queries = @user.price_queries
+    if User.find(params[:id]) != @current_user
+      redirect_to user_path(@current_user.id)
+    else
+      @user = current_user
+      @price_queries = @user.price_queries
+    end
   end
 
   def edit
-    user_id = params[:id]
-    @user = User.find_by_id(user_id)
+    if User.find(params[:id]) != @current_user
+      redirect_to edit_user_path(@current_user.id)
+    else
+      @user = current_user
+    end
   end
 
   def update
-    user_id = params[:id]
-    @user = User.find_by_id(user_id)
-
-    updated_user = params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
-
-    @user.update_attributes(
-      :first_name => updated_user[:first_name],
-      :last_name  => updated_user[:last_name],
-      :email      => updated_user[:email],
-      :password   => updated_user[:password],
-      :password_confirmation   => updated_user[:password_confirmation]
-    )
-
-    redirect_to user_path
-
+    if User.find(params[:id]) == current_user
+      @user = current_user
+      updated_user = params.require(:user).permit(
+        :first_name,
+        :last_name,
+        :email,
+        :password,
+        :password_confirmation
+      )
+      @user.update_attributes(
+        :first_name => updated_user[:first_name],
+        :last_name  => updated_user[:last_name],
+        :email      => updated_user[:email],
+        :password   => updated_user[:password],
+        :password_confirmation   => updated_user[:password_confirmation]
+      )
+      redirect_to user_path, :notice => 'Profile updated!'
+    else
+      session[:user_id] = nil
+      redirect_to login_path, :notice => 'Please re-login.'
+    end
   end
 
   def destroy
-    user_id = params[:id]
-    @user = User.find_by_id(user_id)
-    @user.destroy
-    redirect_to root_path
+    if User.find(params[:id]) == current_user
+      @user = current_user
+      @user.destroy
+      session[:user_id] = nil
+      redirect_to root_path, :notice => 'Profile deleted.'
+    else
+      session[:user_id] = nil
+      redirect_to login_path, :notice => 'Please re-login.'
+    end
   end
 
 end
