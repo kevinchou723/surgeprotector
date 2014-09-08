bfa3dc87243dc804590180deabf705e0c158e987
Lisa Vogt <lisavogt.sf@gmail.com>
2014-09-08 16:15:14 -0700

    changes factory to ffaker

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,15 +1,14 @@
 # user.rb
 
 FactoryGirl.define do
-  sequence :email do |n|
-    "user_#{n}@factory.com"
-  end
-
+  # sequence :email do |n|
+  #   "user_#{n}@factory.com"
+  # end
   factory :user do
-    email 
+    email Faker::Internet.email
     first_name "Cameron"
     last_name "Jacoby"
-    pswrd = "blahblah"
+    pswrd = Faker::Lorem.words(4).join("")
     password pswrd
     password_confirmation pswrd
   end
