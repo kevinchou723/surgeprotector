d351f171b37b7b8932d826ad562de2d97c64c661
cameronjacoby <cameron.jacoby@gmail.com>
2014-09-08 17:17:59 -0700

    refactor model tests to use factory girl & ffaker

--- spec/factories/price_query.rb
+++ spec/factories/price_query.rb
@@ -1,1 +1,13 @@
-# price_query.rb
+FactoryGirl.define do
+
+  factory :price_query do 
+    start_latitude Faker::Geolocation.lat
+    start_longitude Faker::Geolocation.lng
+    end_latitude Faker::Geolocation.lat
+    end_longitude Faker::Geolocation.lng
+    city 'San Francisco'
+    nickname 'GA to Union Square'
+    start_address '501 Folsom Street, San Francisco, CA'
+  end
+
+end
