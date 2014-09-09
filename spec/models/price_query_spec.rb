a005ecc57665e95f36fa78f807d0a878e5114309
cameronjacoby <cameron.jacoby@gmail.com>
2014-09-08 17:50:53 -0700

    refactor price query spec to use subject instead of example_query

--- spec/models/price_query_spec.rb
+++ spec/models/price_query_spec.rb
@@ -8,65 +8,37 @@ RSpec.describe PriceQuery, :type => :model do
     expect(subject).to be_valid
   end
 
-  it 'should validate presence and numbericality of start points' do
-    example_query_sf = PriceQuery.create({
+  it 'should validate presence and numericality of start points' do
+    example_query = PriceQuery.create({
       :city => 'San Francisco',
       :nickname => 'GA to Union Square'
     })
-    expect(example_query_sf.errors.size).to eql(4)
+    expect(example_query.errors.size).to eql(4)
   end
 
   it 'should validate that start points are within latitude/longitude range' do
-    example_query_sf = PriceQuery.create({
+    example_query = PriceQuery.create({
       :start_latitude => 100,
       :start_longitude => -200,
-      :end_latitude => -150,
-      :end_longitude => 300,
       :city => 'San Francisco',
       :nickname => 'GA to Union Square'
     })
-    expect(example_query_sf.errors.size).to eql(2)
+    expect(example_query.errors.size).to eql(2)
   end
 
   it 'should have many price results' do
-    example_query_sf = PriceQuery.create({
-      :start_latitude => 37.786958,
-      :start_longitude => -122.394462,
-      :end_latitude => 37.787933,
-      :end_longitude => -122.4074981,
-      :city => 'San Francisco',
-      :nickname => 'GA to Union Square'
+    price_result = PriceResult.create({
+      :localized_display_name => 'uberX',
+      :low_estimate =>'5',
+      :display_name =>'uberX',
+      :product_id =>'a1111c8c-c720-46c3-8534-2fcdd730040d',
+      :surge_multiplier =>1.0,
+      :estimate =>'$5-7',
+      :high_estimate =>'7',
+      :currency_code =>'USD'
     })
-    example_query_sf.price_results.create({
-      'localized_display_name'=>'uberX',
-      'low_estimate'=>'5',
-      'display_name'=>'uberX',
-      'product_id'=>'a1111c8c-c720-46c3-8534-2fcdd730040d',
-      'surge_multiplier'=>1.0,
-      'estimate'=>'$5-7',
-      'high_estimate'=>'7',
-      'currency_code'=>'USD'
-    })
-    expect(example_query_sf.price_results.length).to eql(1)
-  end
-
-  it 'should associate price query with a user' do
-    cameron = User.create({
-      :email => 'cameron@gmail.com',
-      :first_name => 'Cameron',
-      :last_name => 'Jacoby',
-      :password => '123456',
-      :password_confirmation => '123456'
-    })
-    example_query_sf = cameron.price_queries.create({
-      :start_latitude => 37.786958,
-      :start_longitude => -122.394462,
-      :end_latitude => 37.787933,
-      :end_longitude => -122.4074981,
-      :city => 'San Francisco',
-      :nickname => 'GA to Union Square'
-    })
-    expect(cameron.price_queries.length).to eql(1)
+    subject.price_results << price_result
+    expect(subject.price_results.length).to eql(1)
   end
 
   it 'should belong to a user' do
@@ -77,15 +49,9 @@ RSpec.describe PriceQuery, :type => :model do
       :password => '123456',
       :password_confirmation => '123456'
     })
-    example_query_sf = cameron.price_queries.create({
-      :start_latitude => 37.786958,
-      :start_longitude => -122.394462,
-      :end_latitude => 37.787933,
-      :end_longitude => -122.4074981,
-      :city => 'San Francisco',
-      :nickname => 'GA to Union Square'
-    })
-    expect(example_query_sf.user).to eql(cameron)
+    cameron.price_queries << subject
+    expect(cameron.price_queries.length).to eql(1)
+    expect(subject.user).to eql(cameron)
   end
 
 end
